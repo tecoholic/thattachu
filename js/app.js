43254f52566edaab8b17ba2e7769876fc05262d6
tecoholic <arun@arunmozhi.in>
2015-05-19 20:56:27 +0530

    course lessons load based in user selection

--- js/app.js
+++ js/app.js
@@ -9,27 +9,57 @@ $(document).ready(function(){
         }
     });
 
-    // populate the courses upon language selection
-    $('#lang').on('change', function(){
-        if($("#lang").val() !== 'none'){
-            $.getJSON("/data/"+$('#lang').val()+"/courselist.json", function(d){
-                for(var i=0; i<d.length; i++){
-                    $('#courseSelect').append("<option value='"+d[i].file+"'>"+d[i].name+"</option>");
-                }
-            });
-        }
-    });
+});
 
-    // Load the course into the tutor upon course selection
-    $('#courseSelect').on('change', function(){
-        $('#myModal').modal('hide');
-        $.getJSON("/data/"+$("#lang").val()+"/"+$('#courseSelect').val(),function(course){
-            // Load the lessons list to the sidebar
-            $('#sidebarList').empty();
-            for(var i=0; i < course.lessons.length; i++){
-                $('#sidebarList').append('<li><a href="#">'+course.lessons[i].name+'</a></li>');
+var tch = {};
+tch.courseData = {};
+
+tch.loadLesson = function(lname){
+    if(!tch.courseData.name){
+            return;
+    }
+    var l = tch.courseData.lessons;
+    for(var i=0; i < l.length; i++){
+        if(l[i].name === lname){
+            $('#instructions').html(l[i].instructions);
+            var lines = l[i].lines;
+            $('#workbench').empty();
+            for( var i=0; i<lines.length; i++){
+                $('#workbench').append('<p class="line">'+lines[i]+'</p><textarea rows=1 cols=80 class="typespace"></textarea>');
+            }
+            return;
+        }
+    }
+}
+
+// populate the courses upon language selection
+$('#lang').on('change', function(){
+    if($("#lang").val() !== 'none'){
+        $.getJSON("/data/"+$('#lang').val()+"/courselist.json", function(d){
+            for(var i=0; i<d.length; i++){
+                $('#courseSelect').append("<option value='"+d[i].file+"'>"+d[i].name+"</option>");
             }
         });
-    });
-
+    }
 });
+
+
+// Load the course into the tutor upon course selection
+$('#courseSelect').on('change', function(){
+    $('#myModal').modal('hide');
+    $.getJSON("/data/"+$("#lang").val()+"/"+$('#courseSelect').val(),function(course){
+        // Load the lessons list to the sidebar
+        $('#sidebarList').empty();
+        for(var i=0; i < course.lessons.length; i++){
+            $('#sidebarList').append('<li><a href="#">'+course.lessons[i].name+'</a></li>');
+            // bind the new elements to load the lesson when clicked
+            $('#sidebarList a').click(function(){
+                tch.loadLesson($(this).text());
+            });
+        }
+        tch.courseData = course;
+        tch.loadLesson(course.lessons[0].name);
+    });
+});
+
+
--- tests/entry-test.js
+++ tests/entry-test.js
@@ -1,8 +1,10 @@
-casper.test.begin('Thattachu Entry modal loading', 8, function suite(test){
+casper.test.begin('Thattachu Entry modal loading', 14, function suite(test){
+
+    // open the page and make sure that it asks for language and course
     casper.start("http://0.0.0.0:8000/", function(){
         test.assertTitle("Thattachu", "Yeah! Thattachu is being served");
         casper.waitForResource('jquery.min.js',function(){
-            casper.wait(100,function(){
+            casper.waitUntilVisible('#myModal', function(){
                 test.assertVisible('#myModal', 'The entry form is visible');
             });
         });
@@ -10,7 +12,7 @@ casper.test.begin('Thattachu Entry modal loading', 8, function suite(test){
         test.assertEquals(this.getFormValues('form#entryForm').course, 'none', "No course selected");
     });
 
-
+    // fill in the language and course
     casper.then(function(){
         this.fill('form#entryForm', {'language' : 'en'});
         test.assertEquals(this.getFormValues('form#entryForm').language, 'en', "Language is set to English");
@@ -20,13 +22,31 @@ casper.test.begin('Thattachu Entry modal loading', 8, function suite(test){
         });
     });
 
+    // the modal dialog is closed and the lessons are loaded into the sidebar
+    // and the first lesson by default is opened
     casper.then(function(){
         casper.waitWhileVisible('#myModal', function(){
             test.assertNotVisible('#myModal', 'The entry form is closed');
         });
         casper.waitForResource('demo.json', function(){
-            test.assertElementCount('#sidebar li', 2, 'The sidebar list populated');
+            test.assertElementCount('#sidebar li a', 2, 'The sidebar list populated');
+            test.assertEquals(casper.getHTML('#instructions'),
+                "Welcome to the first lesson in QWERTY Touch Typing. Keep your left index finger on <kbd>f</kbd> and your right index finger on <kbd>j</kbd>.",
+                "The first lesson instruction is loaded.");
+            // there should be as many lines and typespaces as in the json
+            test.assertElementCount('p.line', 6);
+            test.assertElementCount('textarea.typespace', 6);
         });
+    });
+
+    // The instructions and the workbench should be populated as per the lesson selection
+    casper.then(function(){
+        this.clickLabel("Middle DK", "a");
+        test.assertEquals(casper.getHTML('#instructions'),
+            "This is the second lesson of the QWERTY. Keep the left index finger on <kbd>d</kbd> and yout right index finger on <kbd>k</kbd>.",
+            "Instructions change on click");
+        test.assertElementCount('p.line', 7, "Update the key lines");
+        test.assertElementCount('textarea.typespace', 7, "Update the type spaces");
     });
 
     casper.run(function(){
