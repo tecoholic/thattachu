24a28ccf246af0e641eb36b46641008cae4f3832
tecoholic <arun@arunmozhi.in>
2015-05-20 19:55:55 +0530

    updated relative imports

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     // Show the course selection modal
     $('#myModal').modal({show: true, backdrop: 'static', keyboard: false});
 
-    $.getJSON("/data/languages.json", function(d){
+    $.getJSON("./data/languages.json", function(d){
         for(var i=0; i<d.length; i++){
             $('#lang').append("<option value='"+d[i].code+"'>"+d[i].name+"</option>");
         }
@@ -48,7 +48,7 @@ tch.updateAccuracy = function(){
 // populate the courses upon language selection
 $('#lang').on('change', function(){
     if($("#lang").val() !== 'none'){
-        $.getJSON("/data/"+$('#lang').val()+"/courselist.json", function(d){
+        $.getJSON("./data/"+$('#lang').val()+"/courselist.json", function(d){
             for(var i=0; i<d.length; i++){
                 $('#courseSelect').append("<option value='"+d[i].file+"'>"+d[i].name+"</option>");
             }
@@ -59,7 +59,7 @@ $('#lang').on('change', function(){
 // Load the course into the tutor upon course selection
 $('#courseSelect').on('change', function(){
     $('#myModal').modal('hide');
-    $.getJSON("/data/"+$("#lang").val()+"/"+$('#courseSelect').val(),function(course){
+    $.getJSON("./data/"+$("#lang").val()+"/"+$('#courseSelect').val(),function(course){
         // Load the lessons list to the sidebar
         $('#sidebarList').empty();
         for(var i=0; i < course.lessons.length; i++){
