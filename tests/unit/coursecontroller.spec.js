53d29fd3addc21755d7c0ce9061dc2b735772ea1
tecoholic <arun@arunmozhi.in>
2015-06-03 00:15:23 +0530

    refactored course controller test

--- tests/unit/coursecontroller.spec.js
+++ tests/unit/coursecontroller.spec.js
@@ -1,64 +1,16 @@
 describe("CourseController", function(){
     beforeEach( module("thattachu") );
 
-    var $controller, courseData;
+    var $controller, courseData, $scope, controller;
     beforeEach(inject(function(_$controller_, _courseData_){
         $controller = _$controller_;
         courseData = _courseData_;
+
+        $scope = {};
+        controller = $controller( "CourseControl", { $scope: $scope });
     }));
 
-    describe("courseData service functions", function(){
-        beforeEach(inject(function(_courseData_){
-            courseData = _courseData_;
-        }));
-
-        it("should have the get and set courseFilename functions",function(){
-            expect(courseData.getCourseFilename).toBeDefined();
-            expect(courseData.setCourseFilename).toBeDefined();
-        });
-
-
-        describe("just the set function", function(){
-            beforeEach(function(){
-                spyOn( console, "error" );
-            });
-
-            it("should log error on NO data", function(){
-                courseData.setCourseFilename();
-                expect( console.error ).toHaveBeenCalled();
-            });
-
-            it("should log error on only one parameter", function(){
-                courseData.setCourseFilename( "en" );
-                expect( console.error ).toHaveBeenCalled();
-            });
-
-            it( "should log error when any parameter is undefined", function(){
-                var b;
-                courseData.setCourseFilename( "en", b);
-                expect( console.error ).toHaveBeenCalled();
-                courseData.setCourseFilename( b, "en" );
-                expect( console.error ).toHaveBeenCalled();
-            });
-        });
-
-    });
-
-    // Assuming jQuery IME data to be sane and not mocking them here
-    describe("Languages, inputs, courses", function(){
-
-        var $scope, controller;
-        var store = {};
-
-        beforeEach(function(){
-            $scope = {};
-            controller = $controller( "CourseControl", { $scope: $scope });
-        });
-
-        it( "should load all the input methods of the set language", function(){
-            expect($scope.languages.length).not.toEqual( 0 );
-        } );
-
+    describe("> fucntion populateCourses", function(){
         it( "should populate input methods of a language when selected", function(){
             $scope.lang = "ta";
             $scope.populateInputMethods();
@@ -68,13 +20,14 @@ describe("CourseController", function(){
             expect($scope.methods.length).toEqual( 2 );
         });
 
+    });
+
+    describe( "> function setCourse", function(){
         it("should save course data local storage and courseData service", function(){
             $scope.lang = "en";
             $scope.setCourse({ "file" : "demo.json"});
             expect(localStorage.getItem("currentCourse")).toMatch( /demo.json/ );
             expect( courseData.getCourseFilename().filename ).toMatch( /demo.json/ );
         });
-
     });
-
 });
